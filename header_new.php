5f6621c96c74bca56f81b88423955506daec6fab
Patthachaput <>
2021-06-07 14:54:10 +0700

    Add new header

---
+++ header_new.php
@@ -0,0 +1,71 @@
+ <!-- ======= Header normal Page ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Mentor</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="courses.html">Courses</a></li>
+          <li><a href="trainers.html">Trainers</a></li>
+
+          <li class="dropdown"><a href="#"><span>More</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">MyProfile</a></li>
+              <li><a href="#">BookCouse</a></li>
+              <li><a href="#">Topup</a></li>
+              <li><a href="#">MyAllCouse</a></li> <!-- for tutor only -->
+              <li><a href="#">AddCouse</a></li> <!-- for tutor only -->
+              <li><a href="#">SetSchedule</a></li> <!-- for tutor only -->
+              <li><a href="#">AdminPage</a></li> <!-- for admin only -->
+            </ul>
+          </li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="courses.html" class="get-started-btn">Login</a>
+
+    </div>
+  </header><!-- End Header -->
+
+
+  <!-- ======= Header admin Page ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Mentor</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html">Home-Admin</a></li>
+          <li><a href="#">SetSchedule</a></li>
+          <li><a href="#">BookSchedule</a></li>
+          <li><a href="#">Topup</a></li>
+         
+
+          <li class="dropdown"><a href="#"><span>More</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Profile</a></li>
+              <li><a href="#">ViewAllCouse</a></li>
+              <li><a href="#">ViewAllSchedule</a></li>
+              <li><a href="#">ViewAllStudent</a></li>
+              <li><a href="#">ViewAllTutor</a></li>
+            </ul>
+          </li>
+          <li><a href="#">MyProfile</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="courses.html" class="get-started-btn">Login</a>
+
+    </div>
+  </header><!-- End Header -->
